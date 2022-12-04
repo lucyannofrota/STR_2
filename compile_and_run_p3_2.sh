011b27c55567cccfe361ba7cd252593e55465119
Lucyanno Frota <lucyannofrota@gmail.com>
2022-12-04 18:13:47 +0000

    Scripts [Fix]

--- compile_and_run_p3_2.sh
+++ compile_and_run_p3_2.sh
@@ -2,6 +2,8 @@
 #!/bin/sh
 
 cd ros_foxy
-colcon build
+source /opt/ros/foxy/setup.bash
+colcon build --symlink-install
+. install/local_setup.bash
 
-ros2 launch str_ROS2 launch_strNode_1.py
+ros2 launch str_ROS2 launch_strNode_2.py
